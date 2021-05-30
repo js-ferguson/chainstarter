f1f5365fdddad992c904b4dd9011c5967d73ad8f
js-ferguson <jimi.windmills@gmail.com>
2021-05-30 09:17:38 +0200

    added new pages to list spending requests and create new spending requests

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0xd8B6EC5BAe2EB97593b8F7F815383e7b94e06C8E'
+    '0x3A444C1A5A9824626ceCf7beD737c77E3edf8a2C'
 );
 
 export default instance;
